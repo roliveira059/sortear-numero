e13a1711fdffc0aa1eca33d5f02ad547920e8916
roliveira059 <roliveira059@gmail.com>
2024-01-19 19:39:48 -0300

    Adicinado validação para o primeiro núemero ser menor que o segundo

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (max - min +1 )) + min;
-    
-    alert(result)
+
+    if(min < max){
+        const result = Math.floor(Math.random() * (max - min +1 )) + min;
+        alert(result)
+    }else { 
+        alert("O primeiro número deve ser menor que o segundo!")
+    }
 }
